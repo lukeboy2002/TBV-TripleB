d1b50d7581715c348ccf19cfe643686ea3e593f2
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2024-02-06 23:06:10 +0100

    created migration PostView

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\PostView;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
@@ -13,8 +14,16 @@ class PostController extends Controller
 
     }
 
-    public function show(Post $post): View
+    public function show(Post $post, Request $request): View
     {
+//        $user = $request->user();
+//        PostView::create([
+//            'ip_address' => $request->ip(),
+//            'user_agent' => $request->userAgent(),
+//            'post_id' => $post->id,
+//            'user_id' => $user?->id
+//        ]);
+
         return view('posts.show', [
             'post' => $post
         ]);
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <x-slot name="header">
+
+    </x-slot>
+
+    {!! $post->body !!}
+
+    <x-slot name="side">
+        SIDE
+    </x-slot>
+</x-app-layout>
