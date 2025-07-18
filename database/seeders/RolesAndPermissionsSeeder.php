b1da1e1949f71422e9d39fcf2c328bcd6c9ead4f
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2025-07-18 16:37:00 +0200

    Update RolesAndPermissionsSeeder.php

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -18,32 +18,32 @@ class RolesAndPermissionsSeeder extends Seeder
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
         // Misc
-        $miscPermission = Permission::create(['name' => 'N/A']);
+        $miscPermission = Permission::create(['name' => 'N/A', 'guard_name' => 'web']);
 
         // USER MODEL
-        $userPermission1 = Permission::create(['name' => 'create:user']);
-        $userPermission2 = Permission::create(['name' => 'show:user']);
-        $userPermission3 = Permission::create(['name' => 'update:user']);
-        $userPermission4 = Permission::create(['name' => 'delete:user']);
+        $userPermission1 = Permission::create(['name' => 'create:user', 'guard_name' => 'web']);
+        $userPermission2 = Permission::create(['name' => 'show:user', 'guard_name' => 'web']);
+        $userPermission3 = Permission::create(['name' => 'update:user', 'guard_name' => 'web']);
+        $userPermission4 = Permission::create(['name' => 'delete:user', 'guard_name' => 'web']);
 
         // ROLE MODEL
-        $rolePermission1 = Permission::create(['name' => 'create:role']);
-        $rolePermission2 = Permission::create(['name' => 'show:role']);
-        $rolePermission3 = Permission::create(['name' => 'update:role']);
-        $rolePermission4 = Permission::create(['name' => 'delete:role']);
+        $rolePermission1 = Permission::create(['name' => 'create:role', 'guard_name' => 'web']);
+        $rolePermission2 = Permission::create(['name' => 'show:role', 'guard_name' => 'web']);
+        $rolePermission3 = Permission::create(['name' => 'update:role', 'guard_name' => 'web']);
+        $rolePermission4 = Permission::create(['name' => 'delete:role', 'guard_name' => 'web']);
 
         // PERMISSION MODEL
-        $permission1 = Permission::create(['name' => 'create:permission']);
-        $permission2 = Permission::create(['name' => 'show:permission']);
-        $permission3 = Permission::create(['name' => 'update:permission']);
-        $permission4 = Permission::create(['name' => 'delete:permission']);
+        $permission1 = Permission::create(['name' => 'create:permission', 'guard_name' => 'web']);
+        $permission2 = Permission::create(['name' => 'show:permission', 'guard_name' => 'web']);
+        $permission3 = Permission::create(['name' => 'update:permission', 'guard_name' => 'web']);
+        $permission4 = Permission::create(['name' => 'delete:permission', 'guard_name' => 'web']);
 
         // CREATE ROLES
-        $userRole = Role::create(['name' => 'user'])->syncPermissions([
+        $userRole = Role::create(['name' => 'user', 'guard_name' => 'web'])->syncPermissions([
             $miscPermission,
         ]);
 
-        $superAdminRole = Role::create(['name' => 'admin'])->syncPermissions([
+        $superAdminRole = Role::create(['name' => 'admin', 'guard_name' => 'web'])->syncPermissions([
             $userPermission1,
             $userPermission2,
             $userPermission3,
@@ -57,7 +57,7 @@ class RolesAndPermissionsSeeder extends Seeder
             $permission3,
             $permission4,
         ]);
-        $memberRole = Role::create(['name' => 'member'])->syncPermissions([
+        $memberRole = Role::create(['name' => 'member', 'guard_name' => 'web'])->syncPermissions([
             $userPermission1,
         ]);
     }
