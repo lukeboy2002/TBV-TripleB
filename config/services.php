37aa4a31172defe9ce5f0f924f0eb54168363f8a
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2025-07-24 19:34:27 +0200

    Update services.php

--- config/services.php
+++ config/services.php
@@ -28,6 +28,8 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'guard' => ['web'],
+
     'slack' => [
         'notifications' => [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
