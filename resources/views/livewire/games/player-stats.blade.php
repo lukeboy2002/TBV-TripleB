a70169cc08dcd4dff3546b2a3cc9263b54c2e1ea
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2025-09-08 08:42:30 +0200

    Change image sizes

--- resources/views/livewire/games/player-stats.blade.php
+++ resources/views/livewire/games/player-stats.blade.php
@@ -37,9 +37,8 @@
                         <td class="px-6 py-4">
                             <div class="flex items-center">
                                 @if($player->profile && $player->profile->image_path)
-                                    <img class="h-10 w-10 rounded-full object-cover mr-3"
+                                    <img class="min-h-10 min-w-10 rounded-full object-cover mr-3"
                                          src="{{ asset('storage/'. $player->profile->image_path) }}"
-                                         {{--                                         src="{{ Storage::url($player->profile->image_path) }}"--}}
                                          alt="{{ $player->name }}">
                                 @endif
                                 <div>
