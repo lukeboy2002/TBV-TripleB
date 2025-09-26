ba1fa2eb62266d627045dd52742c3cf0ef01bdd2
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2025-09-26 21:04:28 +0200

    Update user.blade.php

--- resources/views/components/menu/user.blade.php
+++ resources/views/components/menu/user.blade.php
@@ -1,13 +1,1 @@
-@can('create:post')
-    <x-dropdown-link
-            href="{{ route('posts.create') }}">
-        {{ __('New Post') }}
-    </x-dropdown-link>
-@endcan
 
-@can('create:user')
-    <x-dropdown-link
-            href="{{ route('invitations.index') }}">
-        {{ __('Invite User') }}
-    </x-dropdown-link>
-@endif
