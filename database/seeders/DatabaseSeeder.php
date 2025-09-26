6674de7c18cd6cf79d4e0b1074a3c308605f7c24
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2025-09-26 21:31:36 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -43,14 +45,14 @@ class DatabaseSeeder extends Seeder
         //            ]);
         //        }
 
-        //        $users = User::factory(10)->create();
-        //        foreach ($users as $user) {
-        //            $role = Role::select('id')->where('name', 'user')->first();
-        //            $user->roles()->attach($role);
-        //            $profile = Profile::create([
-        //                'user_id' => $user->id,
-        //            ]);
-        //        }
+        $users = User::factory(10)->create();
+        foreach ($users as $user) {
+            $role = Role::select('id')->where('name', 'user')->first();
+            $user->roles()->attach($role);
+            $profile = Profile::create([
+                'user_id' => $user->id,
+            ]);
+        }ß
 
         //        $posts = Post::factory(200)
         //            ->has(Comment::factory(15)->recycle([$members, $users]))
